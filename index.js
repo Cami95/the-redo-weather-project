c1c7bde92a4c7bc8a2de833d1993358a8f350ade
Cami95 <74115479+Cami95@users.noreply.github.com>
2020-12-17 10:35:06 -0500

    added js

---
+++ index.js
@@ -0,0 +1,16 @@
+function tellUsTheWeather(response) {
+  document.querySelector("#city");
+  city.innerHTML = response.data.name;
+  document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.date.main.temp);
+}
+function search(event) {
+  event.preventDefault();
+  let apiKey = "fbf71df4a4f913e5058a300f39854588";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(tellUsTheWeather);
+}
+let searchForm = document.querySelector("#search-city");
+searchForm.addEventListener("submit", search);
