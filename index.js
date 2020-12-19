c250baa9315d4dbb3aa57a41c5e8089166071b2a
Cami95 <74115479+Cami95@users.noreply.github.com>
2020-12-18 19:39:00 -0500

    updated js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ function tellUsTheWeather(response) {
   document.querySelector("#city");
   city.innerHTML = response.data.name;
   document.querySelector("#temperature");
-  temperature.innerHTML = Math.round(response.date.main.temp);
+  temperature.innerHTML = Math.round(response.data.main.temp);
 }
 function search(event) {
   event.preventDefault();
   let apiKey = "fbf71df4a4f913e5058a300f39854588";
   let city = document.querySelector("#city-input").value;
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(tellUsTheWeather);
 }
